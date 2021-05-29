1fa4ac2d0d6ce6b4a45b394cd7be931848f2b9ef
SAIFUR RAHMAN <saifurrahman@SAIFURs-MacBook-Pro.local>
2021-05-29 19:27:55 +0800

    2nd init

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector("#mobile__menu")
+const menuLinks = document.querySelector(".navbar__menu")
+const navLog = document.querySelector(".nav__logo")
+
+
+// display mobile menu
+conts mobileMenu = () =>{
+     
+}
