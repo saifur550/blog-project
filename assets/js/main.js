c99769b63815896f2de9a9fbcbc71a49e2c4e85f
saifur550 <srstap550@gmail.com>
2021-05-30 09:19:15 +0800

    mobile navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
-const menu = document.querySelector("#mobile__menu")
-const menuLinks = document.querySelector(".navbar__menu")
-const navLog = document.querySelector(".nav__logo")
+const menu = document.querySelector('#mobile__menu')
+const menuLinks = document.querySelector('.navbar__menu')
+const navLog = document.querySelector('#nav__logo')
+const body = document.querySelector('body')
 
 
 // display mobile menu
-conts mobileMenu = () =>{
-     
+const mobileMenu = () => {
+     menu.classList.toggle('is__active')
+     menuLinks.classList.toggle('active')
+     body.classList.toggle('active')
 }
+
+
+ menu.addEventListener('click', mobileMenu)
+
+
